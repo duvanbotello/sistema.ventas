49c9e3aacbb48ee3a1e829e34945a74905119906
Duvan Botello <this.duvan@gmail.com>
2018-08-11 11:03:52 -0500

    validando los parametros que se obtienen por la URL

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+    //Esta es la manera de declarar constatntes
+    const LBS = 'Library/';
+    const VIEWS = 'Viwes/';
+
+    //otra forma de declarar constantes utilizando la funcion define revise por parametro el nombre
+    //de la variable y el valor de la constante
+
+    define('RQ','Resource/');
+    define('URL','http://localhost/svphp/');
+
+?>
--- index.php
+++ index.php
@@ -1,4 +1,25 @@
 <?php
+
+//invoca todo lo que esta en config
+require "config.php";
+
 $url = isset($_GET["url"]) ? $_GET["url"]:"Index/index";
-echo $url;
+
+//el metodo explode funciona como el toquineizzer en java.
+//separo todo lo que este en el vector url por "/"
+$url = explode("/", $url);
+    //verifico si el vector url contiene datos.
+    $controller = "";
+    $method = "";
+    if(isset($url[0])){
+        $controller = $url[0];
+    }
+    if(isset($url[1])){
+        //verifica que no venga vacio la posicion del nombre del metodo que se utilziara.
+        if($url[1] != ''){
+            $method = $url[1];
+        }
+    }
+
+    spl_autoload_register(function($class){});
 ?>
