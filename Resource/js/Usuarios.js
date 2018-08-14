be8846328f2f90e1d4b419550abffe20ccfa6c99
Duvan Botello <this.duvan@gmail.com>
2018-08-14 07:27:39 -0500

    Validando los campos del login con jquery-validation

---
+++ Resource/js/Usuarios.js
@@ -0,0 +1,5 @@
+class Usuarios{
+    constructor(){
+        
+    }
+}
---
+++ Resource/js/app.js
@@ -0,0 +1,10 @@
+/**
+ * CODIGO DE USUARIOS
+ */
+var usuarios = new Usuarios();
+
+
+//utilizando jquery
+$().ready(()=>{
+    $("#login").validate()
+});
